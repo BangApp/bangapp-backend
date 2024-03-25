6e61e5111eab789101e0495183eb090e670c3629
martin kaboja <martinkaboja@gmail.com>
2024-03-25 15:13:51 +0300

    delete video

--- routes/api.php
+++ routes/api.php
@@ -634,10 +634,10 @@ Route::middleware('auth:api')->group(function () {
                 File::move(storage_path('app/' . $challengeImagePath), $deletedChallengeImagePath);
             }
         } else {
-            $videoPath = Str::replaceFirst($appUrl . 'storage/app/', '', $post->video);
-            $deletedVideoName = basename($videoPath);
-            $deletedVideoPath = $deletedPath . '/' . $deletedVideoName;
-            File::move(storage_path('app/' . $videoPath), $deletedVideoPath);
+            $url = $post->image;
+            $parts = explode('/', $url);
+            $uid = $parts[count($parts) - 2]; // Get the second-to-last element
+            deleteVideoApi($uid);
         }
         $post->delete();
         return response(['message' => 'Post deleted successfully'], 200);
@@ -1292,6 +1292,31 @@ Route::middleware('auth:api')->group(function () {
         $notification->post_id = $post_id;
         $notification->save();
     }
+
+    function deleteVideoApi($uid)
+    {
+        $apiUrl = "https://video.bangapp.pro/api/v1/delete-video";
+        
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $apiUrl,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => json_encode(['uid' => $uid]), // Assuming you need to pass the ID in the request body
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                // Add any other headers if required
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        return $response;
+    }
+
     // run this query to change the table so as to use this function
     // ALTER TABLE `users` CHANGE `public_id` `public` BOOLEAN NULL DEFAULT FALSE;
     Route::post('/pinMessage', function (Request $request) {
