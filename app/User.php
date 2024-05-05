d1454802c23b9923d73aab70d665c0d434aa3eed
martin kaboja <martinkaboja@gmail.com>
2024-05-05 20:16:47 +0300

    .

--- app/User.php
+++ app/User.php
@@ -127,10 +127,10 @@ class User extends Authenticatable implements JWTSubject
     public function getFriendsCountAttribute()
     {
         // return $this->friends()->where('user_id',$this->id)->orWhere('friend_id',$this->id)->where('confirmed', true)->count();
-        
-        return friends::where(function($query) use ($this->id) {
-                    $query->where('friend_id', $this->id)
-                          ->orWhere('user_id', $this->id);
+        $user_id = $this->id;
+        return friends::where(function($query) use ($user_id) {
+                    $query->where('friend_id', $user_id)
+                          ->orWhere('user_id', $user_id);
                 })
                 ->where('confirmed', true)
                 ->count();
