ba86e5f599f9c6eb991ac7b8ac4a0fface1bbd68
martin kaboja <martinkaboja@gmail.com>
2024-05-05 19:41:32 +0300

    .

--- app/User.php
+++ app/User.php
@@ -121,7 +121,7 @@ class User extends Authenticatable implements JWTSubject
 
     public function friends()
     {
-        return $this->belongsToMany(User::class, 'friends', 'user_id', 'friend_id')->wherePivot('confirmed', true);
+        return $this->belongsToMany(User::class, 'friends', 'user_id', 'friend_id');
     }
 
     public function getFriendsCountAttribute()
