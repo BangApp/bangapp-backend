2a3efc2b03d5d18609eeb48048eb125cbcd2f6e6
martin kaboja <martinlaurentk@gmail.com>
2024-10-06 17:54:47 +0300

    .

--- app/File.php
+++ app/File.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class File extends Model
 {
     use HasFactory;
+    protected $fillable = ['user_id','file_name','file_path','file_type','file_size','body','pinned','price'];
 }
