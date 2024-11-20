297bfc6fd551e5db5785d11bf5fc02944a8b0520
martin kaboja <martinlaurentk@gmail.com>
2024-11-20 21:48:08 +0300

    flutter wave fix

--- app/Http/Controllers/FlutterwaveController.php
+++ app/Http/Controllers/FlutterwaveController.php
@@ -192,16 +192,18 @@ class FlutterwaveController extends Controller
             if ($response->getStatusCode() == 200) {
                 \Log::info("this is response");
                 \Log::info(json_encode( $response_data ));
-                return  $response_data;
+               
                 if ($response_data['status'] == 'success') {
-                    $withdaw->status = 'pending';
                     \Log::info("this is payload response");
                     \Log::info(json_encode( $response_data['data'] ));
+                    $withdaw->status = 'pending';
                     $withdaw->reference_number = $response_data['data']['id'];
                     $withdaw->save();
                     $pushNotificationService = new \App\Http\Controllers\PushNotificationService();
                     $pushNotificationService->sendPushNotification($user->device_token, $user->name, withdrawMessageNotification(), $withdaw->id, 'withdraw',$user->name,$user->id);
                 }
+
+                return  $response_data;
                 
             } else {
                 return response()->json([
